818d863efc30c0c5a997aea0da7b1088841bf63c
Farcicdev <augustoastolphi@gmail.com>
2025-05-08 18:26:26 -0300

    first commit

---
+++ src/test/java/dev/springBoot/CadastroDeNinja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springBoot.CadastroDeNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
