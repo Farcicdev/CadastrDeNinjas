81b1952c86ea07a464271690a7d0c4dc59b3d032
Farcicdev <augustoastolphi@gmail.com>
2025-06-04 10:16:41 -0300

    refactor:Aplicar ResponseEntity nos metodos da controller para padronizar os retornos HTTP

--- src/main/java/dev/springBoot/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/dev/springBoot/CadastroDeNinja/Missoes/MissoesController.java
@@ -1,9 +1,12 @@
 package dev.springBoot.CadastroDeNinja.Missoes;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 //LOCALHOST:8080/deletar/1
 @RestController//controlar rota
@@ -18,8 +21,9 @@ public class MissoesController {
 
     //Post -- Mandar uma requisisao para o usuario criar algo mandar a informacao
     @PostMapping("/criar")
-    public MissoesDTO criar(@RequestBody MissoesDTO missoesModel)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       {
-        return listmissoes.newMissao(missoesModel);
+    public ResponseEntity<String> criar(@RequestBody MissoesDTO missoesModel)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       {
+        MissoesDTO missoes = listmissoes.newMissao(missoesModel);
+        return ResponseEntity.ok().body(missoes.getNomeMissao()+" criado com sucesso");
     }
     //GET -- Mandar uma requisisao para mostrar algo para o usuario
     @GetMapping("/missoes")
@@ -27,13 +31,33 @@ public class MissoesController {
         return listmissoes.mostrarMissoes();        
     }
 
+    @GetMapping("/missoes/{id}")
+    public ResponseEntity<?> missoesId(@PathVariable Long id){
+        MissoesDTO missoesDTO = listmissoes.mostrarmissoesId(id);
+        if(missoesDTO != null){
+            return ResponseEntity.ok(missoesDTO);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ninja nao existe");
+        }
+    }
+
     @PutMapping("/alterar/{id}")//@Patchvariable quando precisar do id no url colocar essa annotation
-    public MissoesDTO alterarNinjas(@PathVariable Long id,@RequestBody MissoesDTO missoesAtualizada){
-        return listmissoes.alterarMissao(id,missoesAtualizada);
+    public ResponseEntity<String> alterarNinjas(@PathVariable Long id,@RequestBody MissoesDTO missoesAtualizada){
+        MissoesDTO missoesDTO = listmissoes.alterarMissao(id,missoesAtualizada);
+        if (missoesDTO != null){
+            return ResponseEntity.ok("Ninja alterado com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ninja nao encontrado");
+        }
     }
 
     @DeleteMapping("/deleta/{id}")
-    public void deletaMissao(@PathVariable Long id){
-        listmissoes.deleteMissoesId(id);
+    public ResponseEntity<String> deletaMissao(@PathVariable Long id){
+       if (listmissoes.mostrarmissoesId(id) != null){
+           listmissoes.deleteMissoesId(id);
+           return ResponseEntity.ok().body("Ninja deletado");
+       }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ninja nao encontrado");
+       }
     }
 }
